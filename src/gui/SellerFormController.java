409a2ac3a3af11941be304fe6a8948af3ed5dd18
William Alex <will.i.am.002@hotmail.com>
2020-08-24 10:11:17 -0300

    Saving Seller

--- src/gui/SellerFormController.java
+++ src/gui/SellerFormController.java
@@ -1,9 +1,11 @@
 package gui;
 
 import java.net.URL;
+import java.time.Instant;
 import java.time.LocalDate;
 import java.time.ZoneId;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 import java.util.Locale;
 import java.util.Map;
@@ -134,6 +136,24 @@ public class SellerFormController implements Initializable {
 		}
 		obj.setName(txtName.getText());
 
+		if (txtEmail.getText() == null || txtEmail.getText().trim().equals("")) {
+			exception.addError("email", "Field can't be empty");
+		}
+		obj.setEmail(txtEmail.getText());
+
+		if(dpBirthDate.getValue() == null) {
+			exception.addError("birthDate", "Field can't be empty");
+		}else {
+		Instant instant = Instant.from(dpBirthDate.getValue().atStartOfDay(ZoneId.systemDefault()));
+		obj.setBirthDate(Date.from(instant));
+		}
+		if (txtBaseSalary.getText() == null || txtBaseSalary.getText().trim().equals("")) {
+			exception.addError("baseSalary", "Field can't be empty");
+		}
+			obj.setBaseSalary(Utils.tryParseToDouble(txtBaseSalary.getText()));
+		
+		obj.setDepartment(comboBoxDepartment.getValue());	
+			
 		if (exception.getErrors().size() > 0) {
 			throw exception;
 		}
@@ -158,7 +178,7 @@ public class SellerFormController implements Initializable {
 		Constraints.setTextFieldDouble(txtBaseSalary);
 		Constraints.setTextFieldMaxLength(txtEmail, 60);
 		Utils.formatDatePicker(dpBirthDate, "dd/MM/YYYY");
-		
+
 		initializeComboBoxDepartment();
 
 	}
@@ -175,11 +195,11 @@ public class SellerFormController implements Initializable {
 		if (entity.getBirthDate() != null) {
 			dpBirthDate.setValue(LocalDate.ofInstant(entity.getBirthDate().toInstant(), ZoneId.systemDefault()));
 		}
-		if(entity.getDepartment() == null) {
+		if (entity.getDepartment() == null) {
 			comboBoxDepartment.getSelectionModel().selectFirst();
-		}else {
-		comboBoxDepartment.setValue(entity.getDepartment());
-		
+		} else {
+			comboBoxDepartment.setValue(entity.getDepartment());
+
 		}
 	}
 
@@ -195,21 +215,61 @@ public class SellerFormController implements Initializable {
 	private void setErrorsMessage(Map<String, String> errors) {
 		Set<String> fields = errors.keySet();
 
-		if (fields.contains("name")) {
-			labelErrorName.setText(errors.get("name"));
-		}
+		
+		labelErrorName.setText((fields.contains("name") ? errors.get("name") : "" ));
+		
+		labelErrorEmail.setText((fields.contains("email") ? errors.get("email") : "" ));
+		
+		labelErrorBirthDate.setText((fields.contains("birthDate") ? errors.get("birthDate") : "" ));
+		
+		labelErrorBaseSalary.setText((fields.contains("baseSalary") ? errors.get("baseSalary") : "" ));
+		
+	
+		
+		
+		
+		
+		
+		
+		
+		
+		
+//		if (fields.contains("name")) {
+//			labelErrorName.setText(errors.get("name"));
+//		}else {
+//			labelErrorName.setText("");
+//		}
+//		
+//		if (fields.contains("email")) {
+//			labelErrorEmail.setText(errors.get("email"));
+//		}else {
+//			labelErrorEmail.setText("");
+//		}
+//		
+//		if (fields.contains("baseSalary")) {
+//			labelErrorBaseSalary.setText(errors.get("baseSalary"));
+//		}else {
+//			labelErrorBaseSalary.setText("");
+//		}
+//		
+//		if (fields.contains("birthDate")) {
+//			labelErrorBirthDate.setText(errors.get("birthDate"));
+//		}else {
+//			labelErrorName.setText("");
+//		}
+//		
 	}
 
-	private void initializeComboBoxDepartment() { 
-		Callback<ListView<Department>, ListCell<Department>> factory = lv -> new ListCell<Department>() { 
-			
+	private void initializeComboBoxDepartment() {
+		Callback<ListView<Department>, ListCell<Department>> factory = lv -> new ListCell<Department>() {
+
 			@Override
-			protected void updateItem(Department item, boolean empty) { 
-				super.updateItem(item, empty);   
-				setText(empty ? "" : item.getName());     
-				}   
-			}; 
-			comboBoxDepartment.setCellFactory(factory);
-			comboBoxDepartment.setButtonCell(factory.call(null));   
+			protected void updateItem(Department item, boolean empty) {
+				super.updateItem(item, empty);
+				setText(empty ? "" : item.getName());
 			}
+		};
+		comboBoxDepartment.setCellFactory(factory);
+		comboBoxDepartment.setButtonCell(factory.call(null));
+	}
 }
